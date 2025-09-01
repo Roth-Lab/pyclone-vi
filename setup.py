087027dd45c366d1bdb1e7dcb546872063c292ef
elhurtado <74883583+elhurtado@users.noreply.github.com>
2025-09-01 07:43:57 -0700

    Adding environment.yaml. Updating maintainers list in pyproject.toml. Simplifying setup.py stub file.

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-# from setuptools import find_packages, setup
-#
-# setup(
-#     name="pyclone-vi",
-#     version="0.1.6",
-#     description="Fast method for inferring clonal population structure from SNV data.",
-#     author="Andrew Roth",
-#     author_email="aroth@bccrc.ca",
-#     url="https://github.com/Roth-Lab/pyclone-vi",
-#     packages=find_packages(),
-#     license="GPL v3",
-#     entry_points={
-#         "console_scripts": [
-#             "pyclone-vi= pyclone_vi.cli:main",
-#         ]
-#     },
-# )
-
-
 from setuptools import setup
 
 setup()
